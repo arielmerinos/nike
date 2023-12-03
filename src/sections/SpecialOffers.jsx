411b7a9187cfef3f382e4348483f82a54c93d652
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-02 18:24:04 -0600

    update servicecard component

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -1,7 +1,32 @@
+import { offer } from "../assets/images"
+import Button from "../components/Button"
+
+
 
 const SpecialOffers = () => {
   return (
-    <div>SpecialOffers</div>
+    <section className="flex justify-wrap items-center max-xl:flex-col-reverse gap-10 max-container">
+    <div className="flex-1">
+      <img src={offer}
+        width={773}
+        height={687}
+        className="object-contain w-full"
+      />
+    </div>
+    <div className="flex flex-1 flex-col">
+    <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg ">
+      <span className="text-coral-red "> Special </span> Offer
+ 
+    </h2>
+    <p className="mt-4 lg:max-w-lg info-text">Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incresible savings, we offer unparalleled value that sets us apart</p>
+    <p className="mt-6 lg:max-w-lg info-text"> Navigate a realm of possibilities desgigned to fulfill your unique desires, surpassing the liftiest expectations. Your journey with us is nothing short of excepcional</p>
+    <div className="mt-11 flex gap-2">
+      <Button label="View details"/>
+      <button className="border-2 border-slate-300 text-slate-gray rounded-full p-4 px-4" >Learn more </button>
+    </div>
+  </div>
+
+    </section>
   )
 }
 
