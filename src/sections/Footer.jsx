a86f159c7080ce2d8ab40f67a1a4b97e8f766beb
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-02 22:24:36 -0600

    Add copyright sign and terms & conditions link to footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,3 +1,4 @@
+import { copyrightSign } from "../assets/icons";
 import { footerLogo } from "../assets/images";
 import { footerLinks, socialMedia } from "../constants";
 
@@ -29,12 +30,19 @@ function Footer() {
             <h4 className="text-white font-montserrat text-2xl font-semibold ">{section.title} </h4>
             <ul>
               {section.links.map((link) => (
-                <li key={link} className="text-white text-sm leading-7 font-montserrat mt-4"><a href={link.link}>{link.name}</a></li>
+                <li key={link.name} className="text-white text-sm leading-7 font-montserrat mt-4"><a href={link.link}>{link.name}</a></li>
               ))}
             </ul>
           </div>
           ))}
       </div>
+      </div>
+      <div className="flex justify-between text-white-400 mt-24 max-sm:flex-col max-sm:items-center">
+          <div className="flex flex-1 justify-start items-center gap-2 font-montserrat cursor-pointer">
+            <img src={copyrightSign} alt="copytrithg" width={20} height={20} className="rounded-full m-0"/>
+            <p>Copyright. All rights reserved</p>
+          </div>
+          <p className="font-montserrat cursor-pointer">Terms & conditions</p>
       </div>
     </footer>
   )
