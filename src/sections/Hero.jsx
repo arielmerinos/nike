1175c6594ed3eed93af6c48881fcfd904c319a19
arielmerinos <arielmerino@ciencias.unam.mx>
2023-12-02 19:27:32 -0600

    Update ServiceCard component

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -14,7 +14,7 @@ const Hero = () => {
     id="home"
     className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     > 
-      <div className="relative xl:w-2/5 flex sm:flex-col justify-center items-start w-full xl:padding-x pt-28"> 
+      <div className="relative xl:w-2/5 flex sm:flex-col flex-col p-4 justify-center items-start w-full xl:padding-x mt-8 pt-28 sm:w-full sm:flex-wrap"> 
         <p className="text-lg font-montserrat text-coral-red">Our summer collection</p>
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10"> The new arrival</span>
@@ -24,7 +24,7 @@ const Hero = () => {
         <p className="font-montserrat text-slate-gray py-8 text-lg leading-8 sm:max-w-sm">Dicover stylish Nike arrivals, quality comfort, and innovation for your active life</p>
         <Button label="Shop now!" iconURL={arrowRight}/>
 
-        <div className="flex justify-between items-start flex-wrap w-full  mt-20 gap-8 ">
+        <div className="flex sm:justify-between justify-center items-start flex-wrap w-full  mt-20 gap-8 ">
           {statistics.map(({ value, label }) => (
             <div key={`${value}${label}`} className="flex flex-col justify-center items-center">
               <p className="text-4xl font-bold font-palanquin">{value}</p>
@@ -41,7 +41,7 @@ const Hero = () => {
         height={610}
         width={500}
          />
-         <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6" id="shoes">
+         <div className="flex sm:gap-6 gap-4 absolute sm:-bottom-[5%] -bottom-[20%] mt-2 sm:left-[10%] max-sm:px-6" id="shoes">
           {
             shoes.map((shoe) =>{ 
             return(
